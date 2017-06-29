07711c8af3bf42db51cf3f51ce63eda46409c861
Erik <erik.langenborg@gmail.com>
2017-06-28 20:07:12 -0400

    Initial commit

---
+++ include/cpp_sorter.h
@@ -0,0 +1,7 @@
+
+// simple sorter for arrays
+template <typename T>
+void array_sort(T *arr, size_t len)
+{
+  std::sort(arr, arr + len);
+}
---
+++ include/functions.h
@@ -0,0 +1,12 @@
+
+/**
+  Adds two integers
+
+  @param a the first value
+  @param b the second value
+  @return the sum
+*/
+int int_addition(int a, int b)
+{
+  return a + b * 2;
+}
